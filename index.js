3df9286e66e5d840d3d4f45106b3c97dfc8c1f42
kofi kakah <kofikakah10@gmail.com>
2026-04-08 09:16:09 -0700

    counter-app

--- index.js
+++ index.js
@@ -3,17 +3,33 @@
         const incrementButton = document.getElementById('increment');
         const decrementButton = document.getElementById('decrement');
         const resetButton = document.getElementById('reset');
+        const resetTimesElement = document.getElementById('reset-times');
+        const decrementTimesElement = document.getElementById('decrement-times');
+        const incrementTimesElement = document.getElementById('increment-times');
+        let resetTimes = 0;
+        let decrementTimes = 0;
+        let incrementTimes = 0;
+
 
         incrementButton.addEventListener('click', () => {
             count++;
             counterElement.textContent = count;
+            incrementTimes++;
+            incrementTimesElement.textContent = incrementTimes;
         });
 
         decrementButton.addEventListener('click', () => {
             count--;
             counterElement.textContent = count;
+            decrementTimes++;
+            decrementTimesElement.textContent = decrementTimes;
         });
+
         resetButton.onclick = () => {
+            if (count !== 0) {
+                resetTimes++;
+                resetTimesElement.textContent = resetTimes;
+            }
             count = 0;
             counterElement.textContent = count;
         }
